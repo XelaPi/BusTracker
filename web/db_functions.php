4eca93cbed74b3f4d0f1d5e3a0e0f76e8b6e2e9e
Alex <vanyoalex@gmail.com>
2016-02-19 18:33:19 -0600

    Corrected db_functions to use url_fetch instead of curl

--- web/db_functions.php
+++ web/db_functions.php
@@ -1,31 +1,28 @@
 <?php
-//Generic php function to send GCM push notification
+
 function sendMessageThroughGCM($registration_ids, $message) {
-	//Google cloud messaging GCM-API url
-	$url = 'https://android.googleapis.com/gcm/send';
-	$fields = array(
-		'registration_ids' => $registration_ids,
-		'data' => $message,
-	);
-	define("GOOGLE_API_KEY", "AIzaSyD2TpBIlbAdT0F2y-eWIUwOVq2Y_neaVpM");
-	$headers = array(
-		'Authorization: key=' . GOOGLE_API_KEY,
-		'Content-Type: application/json'
-	);
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);	
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-	$result = curl_exec($ch);				
-	if ($result === FALSE) {
-		die('Curl failed: ' . curl_error($ch));
-	}
-	curl_close($ch);
-	return $result;
+    $fields = array(
+        'registration_ids' => $registration_ids,
+        'data' => $message,
+    );
+    $fields = json_encode($fields);
+    $arrContextOptions = array(
+        "http" => array(
+            "method" => "POST",
+            "header" =>
+                'Authorization: key = AIzaSyD2TpBIlbAdT0F2y-eWIUwOVq2Y_neaVpM'. "\r\n" .
+                'Content-Type: application/json'. "\r\n",
+            "content" => $fields,
+        ),
+        "ssl" => array(
+            "allow_self_signed" => true,
+            "verify_peer" => false,
+        ),
+    );
+    $arrContextOptions = stream_context_create($arrContextOptions);
+    $result = file_get_contents('https://android.googleapis.com/gcm/send', false, $arrContextOptions);
+
+    return $result;
 }
 
 class DB_FUNCTIONS {
