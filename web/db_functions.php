3c1e67f92914e8693e111af9e8fb6a2af24a1cd5
Alex <vanyoalex@gmail.com>
2016-02-22 11:45:32 -0600

    Formatted db_functions.php, put JSON encoding into request PHP

--- web/db_functions.php
+++ web/db_functions.php
@@ -29,165 +29,150 @@ class DB_FUNCTIONS {
 	private $db;
 	
 	function __construct() {
-	require_once __DIR__ . '/db_connect.php';
-	
-	$this->db = new DB_CONNECT();
-}
+		require_once __DIR__ . '/db_connect.php';
 
-function __destruct() {
+		$this->db = new DB_CONNECT();
+	}
 
-}
+	function __destruct() {
 
-public function getRow($id, $number) {
-	$query = sprintf("SELECT * FROM bus_rows WHERE school_id='%s' AND number='%s'", mysql_real_escape_string($id), mysql_real_escape_string($number));
+	}
 
-	$result = mysql_query($query);
-	if ($result) {
-		while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-			return $row['row'];
+	public function getRow($id, $number) {
+		$query = sprintf("SELECT * FROM bus_rows WHERE school_id='%s' AND number='%s'", mysql_real_escape_string($id), mysql_real_escape_string($number));
+
+		$result = mysql_query($query);
+		if ($result) {
+			while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+				return $row['row'];
+			}
+	}
+		return 0;
+	}
+
+	public function addRegId($regId) {
+		$query = sprintf("INSERT INTO reg_ids (reg_id) VALUES('%s')", mysql_real_escape_string($regId));
+
+		$result = mysql_query($query);
+		return $result;
+	}
+
+	public function getAllRegIds() {
+			$query = sprintf("SELECT * FROM reg_ids");
+
+		$result = mysql_query($query);
+
+			$gcmRegIds = array();
+
+			while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+				array_push($gcmRegIds, $row["reg_id"]);
+			}
+
+		return $gcmRegIds;
+	}
+
+	public function addBus($id, $number) {
+		$row = $this->getRow($id, $number);
+
+		return $this->addBusWithRow($id, $row, $number);
+	}
+
+	public function addBusWithRow($id, $row, $number) {
+		$query = sprintf("INSERT INTO buses (school_id, row, number) VALUES('%s', '%s', '%s')", mysql_real_escape_string($id), mysql_real_escape_string($row), mysql_real_escape_string($number));
+
+		$result = mysql_query($query);
+
+		if ($result) {
+
+		$sql_row = $this->getSchoolInfo($id);
+		$row_name= explode(";", $sql_row["row_names"])[$row];
+		$position = $this->getBusPosition($id, $row, $number);
+
+			$message = array(
+				"message" => "add_bus",
+				"_id" => $id,
+				"bus_row" => $row,
+				"row_name" => $row_name,
+				"bus_number" => $number,
+				"position" => $position
+			);
+			$response = sendMessageThroughGCM($this->getAllRegIds(), $message);
 		}
-}
-	return 0;
-}
 
-public function addRegId($regId) {
-	$query = sprintf("INSERT INTO reg_ids (reg_id) VALUES('%s')", mysql_real_escape_string($regId));
+		return $response;
+	}
 
-	$result = mysql_query($query);
-	return $result;
-}
+	public function getAllBuses($id) {
+		$query = sprintf("SELECT * FROM buses WHERE school_id='%s' ORDER BY _id", mysql_real_escape_string($id));
 
-public function getAllRegIds() {
-		$query = sprintf("SELECT * FROM reg_ids");
+		$result = mysql_query($query);
 
-	$result = mysql_query($query);
+		return $result;
+	}
 
-		$gcmRegIds = array();
+	public function getBusPosition($id, $row, $number) {
+		$query = sprintf("SELECT COUNT(*)+1 FROM buses WHERE _id<(SELECT _id FROM buses WHERE school_id='%1\$s' AND row='%2\$s' AND number='%3\$s') AND school_id='%1\$s' AND row='%2\$s'",
+				mysql_real_escape_string($id),
+				mysql_real_escape_string($row),
+				mysql_real_escape_string($number));
 
-		while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-			array_push($gcmRegIds, $row["reg_id"]);
+		$result = mysql_fetch_assoc(mysql_query($query))["COUNT(*)+1"];
+		return $result;
+	}
+
+	public function removeAllBuses($id) {
+		$query = sprintf("DELETE FROM buses WHERE school_id='%s'", mysql_real_escape_string($id));
+
+		$result = mysql_query($query);
+
+		if ($result) {
+			$message = array(
+				"message" => "remove_all_buses",
+				"_id" => $id
+			);
+			$response = sendMessageThroughGCM($this->getAllRegIds(), $message);
 		}
 
-	return $gcmRegIds;
-}
+		return $response;
+	}
 
-public function addBus($id, $number) {
-	$row = $this->getRow($id, $number);
+	public function removeBus($id, $row, $number) {
+	$query = sprintf("DELETE FROM buses WHERE school_id='%s' AND number='%s'", mysql_real_escape_string($id), mysql_real_escape_string($number));
 
-	return $this->addBusWithRow($id, $row, $number);
-}
+		$result = mysql_query($query);
 
-public function addBusWithRow($id, $row, $number) {
-	$query = sprintf("INSERT INTO buses (school_id, row, number) VALUES('%s', '%s', '%s')", mysql_real_escape_string($id), mysql_real_escape_string($row), mysql_real_escape_string($number));
+		if ($result) {
 
-	$result = mysql_query($query);
-
-	if ($result) {
-
-	$sql_row = $this->getSchoolInfo($id);
-	$row_name= explode(";", $sql_row["row_names"])[$row];
-	$position = $this->getBusPosition($id, $row, $number);
-
-		$message = array(
-			"message" => "add_bus",
+			$message = array(
+			"message" => "remove_bus",
 			"_id" => $id,
 			"bus_row" => $row,
-			"row_name" => $row_name,
-			"bus_number" => $number,
-			"position" => $position
-		);
-		$response = sendMessageThroughGCM($this->getAllRegIds(), $message);
-	}
-
-	return $response;
-}
-
-public function getAllBuses($id) {
-	$query = sprintf("SELECT * FROM buses WHERE school_id='%s' ORDER BY _id", mysql_real_escape_string($id));
-
-	$result = mysql_query($query);
-
-return $result;
-}
-
-public function getBusPosition($id, $row, $number) {
-$query = sprintf("SELECT COUNT(*)+1 FROM buses WHERE _id<(SELECT _id FROM buses WHERE school_id='%1\$s' AND row='%2\$s' AND number='%3\$s') AND school_id='%1\$s' AND row='%2\$s'", mysql_real_escape_string($id), mysql_real_escape_string($row), mysql_real_escape_string($number));
-
-$result = mysql_fetch_assoc(mysql_query($query))["COUNT(*)+1"];
-return $result;
-}
-
-public function removeAllBuses($id) {
-	$query = sprintf("DELETE FROM buses WHERE school_id='%s'", mysql_real_escape_string($id));
-
-	$result = mysql_query($query);
-
-	if ($result) {
-		$message = array(
-			"message" => "remove_all_buses",
-			"_id" => $id
-		);
-		$response = sendMessageThroughGCM($this->getAllRegIds(), $message);
-	}
-
-return $response;
-}
-
-public function removeBus($id, $row, $number) {
-$query = sprintf("DELETE FROM buses WHERE school_id='%s' AND number='%s'", mysql_real_escape_string($id), mysql_real_escape_string($number));
-
-	$result = mysql_query($query);
-
-	if ($result) {
-
-		$message = array(
-		"message" => "remove_bus",
-		"_id" => $id,
-		"bus_row" => $row,
-			"bus_number" => $number
-		);
-		$response = sendMessageThroughGCM($this->getAllRegIds(), $message);
-	}
-
-return $response;
-}
-
-public function getSchoolInfo($id) {
-	$query = sprintf("SELECT * FROM config WHERE _id='%s'", mysql_real_escape_string($id));
-
-	$result = mysql_query($query);
-	if ($result) {
-		while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-			return $row;
-		}
-}
-	return false;
-}
-
-public function getAllSchools() {
-	$query = sprintf("SELECT * FROM config");
-
-	$result = mysql_query($query);
-
-	if (mysql_num_rows($result) > 0) {
-		$response["success"] = 1;
-		$response["schools"] = array();
-
-		while ($row = mysql_fetch_array($result)) {
-			$school = array();
-			$school["_id"] = $row["_id"];
-			$school["name"] = $row["name"];
-			$school["rows"] = $row["rows"];
-			$school["row_names"] = explode(";", $row["row_names"]);
-			$school["default_row"] = $row["default_row"];
-			array_push($response["schools"], $school);
+				"bus_number" => $number
+			);
+			$response = sendMessageThroughGCM($this->getAllRegIds(), $message);
 		}
 
-	} else {
-		$response["success"] = 0;
+		return $response;
 	}
 
-	return $response;
+	public function getSchoolInfo($id) {
+		$query = sprintf("SELECT * FROM config WHERE _id='%s'", mysql_real_escape_string($id));
+
+		$result = mysql_query($query);
+		if ($result) {
+			while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+				return $row;
+			}
+		}
+		return false;
+	}
+
+	public function getAllSchools() {
+		$query = sprintf("SELECT * FROM config");
+
+		$result = mysql_query($query);
+
+		return $result;
 	}
 }
 ?>
--- web/get_schools.php
+++ web/get_schools.php
@@ -1,7 +1,29 @@
 <?php
 require_once __DIR__ . '/db_functions.php';
 
+$response = array();
+
 $db = new DB_FUNCTIONS();
 
-echo json_encode($db->getAllSchools());
+$result = $db->getAllSchools();
+
+if (mysql_num_rows($result) > 0) {
+	$response["success"] = 1;
+	$response["schools"] = array();
+
+	while ($row = mysql_fetch_array($result)) {
+		$school = array();
+		$school["_id"] = $row["_id"];
+		$school["name"] = $row["name"];
+		$school["rows"] = $row["rows"];
+		$school["row_names"] = explode(";", $row["row_names"]);
+		$school["default_row"] = $row["default_row"];
+		array_push($response["schools"], $school);
+	}
+
+} else {
+	$response["success"] = 0;
+}
+
+echo json_encode($response);
 ?>
